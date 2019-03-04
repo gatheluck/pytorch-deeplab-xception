7ce861f0285133a6b7e6eeaee8a7f53422e3e6e2
Yoshihiro Fukuhara <gatheluck+tech@gmail.com>
2019-03-05 04:32:23 +0900

    support loading backbone weight from path

--- modeling/backbone/__init__.py
+++ modeling/backbone/__init__.py
@@ -1,13 +1,17 @@
+import torch
 from modeling.backbone import resnet, xception, drn, mobilenet
 
-def build_backbone(backbone, output_stride, BatchNorm):
-    if backbone == 'resnet':
-        return resnet.ResNet101(output_stride, BatchNorm)
-    elif backbone == 'xception':
-        return xception.AlignedXception(output_stride, BatchNorm)
-    elif backbone == 'drn':
-        return drn.drn_d_54(BatchNorm)
-    elif backbone == 'mobilenet':
-        return mobilenet.MobileNetV2(output_stride, BatchNorm)
-    else:
-        raise NotImplementedError
+def build_backbone(backbone, weight_bb, output_stride, BatchNorm):
+	if backbone == 'resnet':
+		model = resnet.ResNet101(output_stride, BatchNorm)
+		model._load_pretrained_model(weight_bb)
+		#model.load_state_dict(torch.load(weight_bb))
+		return model
+	elif backbone == 'xception':
+		return xception.AlignedXception(output_stride, BatchNorm)
+	elif backbone == 'drn':
+		return drn.drn_d_54(BatchNorm)
+	elif backbone == 'mobilenet':
+		return mobilenet.MobileNetV2(output_stride, BatchNorm)
+	else:
+		raise NotImplementedError
