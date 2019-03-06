036f633edc895594ce0881623600b781df253119
Yoshihiro Fukuhara <gatheluck+tech@gmail.com>
2019-03-06 16:26:13 +0900

    add abci script

---
+++ abci/resnet101_pth-IN.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#$ -l rt_F=1
+#$ -l h_rt=72:00:00
+#$ -j y
+#$ -N dlab_res101_pth-IN_pascal
+#$ -o /home/aaa10329ah/user/waseda/abci_log/deeplab_resnet101_pth-IN_pascal.o
+
+source /etc/profile.d/modules.sh
+module load cuda/9.0/9.0.176.4
+export PATH="/home/aaa10329ah/anaconda3/bin:${PATH}"
+source activate waseda
+cd /home/aaa10329ah/user/waseda/pytorch-deeplab-xception
+# script
+
+python train.py --backbone resnet50 --bb_weight data/models/resnet101_pth-IN_pascal.pth --epochs 50 -l logs/resnet101_pth-IN_pascal --batch_size 32
---
+++ abci/resnet50_rge-SIN-IN_pascal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#$ -l rt_F=1
+#$ -l h_rt=72:00:00
+#$ -j y
+#$ -N dlab_res50_rge-SIN-IN_pascal
+#$ -o /home/aaa10329ah/user/waseda/abci_log/deeplab_resnet50_rge-SIN-IN_pascal.o
+
+source /etc/profile.d/modules.sh
+module load cuda/9.0/9.0.176.4
+export PATH="/home/aaa10329ah/anaconda3/bin:${PATH}"
+source activate waseda
+cd /home/aaa10329ah/user/waseda/pytorch-deeplab-xception
+# script
+
+python train.py --backbone resnet50 --bb_weight data/models/resnet50_rge-SIN-IN.pth --epochs 50 -l logs/resnet50_rge-SIN-IN_pascal --batch_size 32
