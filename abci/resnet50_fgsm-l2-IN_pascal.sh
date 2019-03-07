0fd86f69356d473a1308d3ce46d3edd1a23411f8
Yoshihiro Fukuhara <gatheluck+tech@gmail.com>
2019-03-08 01:53:21 +0900

    add script for adversarial traing

---
+++ abci/resnet50_fgsm-l2-IN_pascal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#$ -l rt_F=1
+#$ -l h_rt=72:00:00
+#$ -j y
+#$ -N dlab_res50_fgsm-l2-IN_pascal
+#$ -o /home/aaa10329ah/user/waseda/abci_log/deeplab_resnet50_fgsm-l2-IN_pascal.o
+
+source /etc/profile.d/modules.sh
+module load cuda/9.0/9.0.176.4
+export PATH="/home/aaa10329ah/anaconda3/bin:${PATH}"
+source activate deeplab
+cd /home/aaa10329ah/user/waseda/deeplab
+# script
+
+python train.py --backbone resnet50 \
+								--bb_weight data/models/resnet50_fgsm-l2-IN.fgsm-l2 \
+								--epochs 200 \
+								-l logs/resnet50_fgsm-l2-IN_pascal \
+								--batch_size 16
