45a20f2ccac0b7222d91df9942b161c1f12b2b63
Yoshihiro Fukuhara <gatheluck+tech@gmail.com>
2019-03-09 11:44:01 +0900

    add scripts

---
+++ abci/IN_pt_norm_ft_resnet101.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#$ -l rt_F=1
+#$ -l h_rt=24:00:00
+#$ -j y
+#$ -N dlab_IN_pt_norm_ft_resnet101
+#$ -o /home/aaa10329ah/user/waseda/abci_log/dlab_IN_pt_norm_ft_resnet101.o
+
+source /etc/profile.d/modules.sh
+module load cuda/9.0/9.0.176.4
+export PATH="/home/aaa10329ah/anaconda3/bin:${PATH}"
+source activate deeplab
+cd /home/aaa10329ah/user/waseda/deeplab
+# script
+
+python train.py --backbone resnet101 \
+								--bb_weight data/models/IN_pt_norm_ft_resnet101.pth \
+								--epochs 100 \
+								-l logs/IN_pt_norm_ft_resnet101 \
+								--batch_size 16
